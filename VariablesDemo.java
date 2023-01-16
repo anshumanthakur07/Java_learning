48e09fbf5d7d098198c10f02a6b9b12b31eaf936
ANSHUMAN <anshumansinghchauhan786@gmail.com>
2023-01-16 14:30:18 +0530

    commit of variables

---
+++ VariablesDemo.java
@@ -0,0 +1,37 @@
+// public class VariablesDemo {
+//     int x;
+//     static int y;
+//     public static void main(String args[])
+//     {
+//         int z;
+//         z=10;
+//     VariablesDemo ob1=new VariablesDemo();
+//     VariablesDemo ob2=new VariablesDemo();
+//     ob1.x=5;
+//     ob1.y=7;
+//     System.out.println(ob2.x);
+//     System.out.println(ob2.y);
+//     System.out.println(z);
+//     }
+    
+// }
+
+
+public class VariablesDemo {
+    int x;
+    static int y;
+    public static void main(String args[])
+    {
+        int z;
+        z=10;
+    VariablesDemo ob1=new VariablesDemo();
+    VariablesDemo ob2=new VariablesDemo();
+    ob1.x=5;
+    ob1.y=7;
+    ob2.y=15;
+    System.out.println(ob2.x);
+    System.out.println(ob2.y);
+    System.out.println(z);
+    }
+    
+}
